5e3c1679ecd79ad134ba6987744e5e61add8344e
Bezzanin <shoxy95@mail.ru>
2019-10-23 12:57:44 +0300

    Updates

---
+++ precache-manifest.04e5525fc889c27c5cbf578709c379e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f640ae43811db081951587a9478639",
+    "url": "/next-games-challenge/index.html"
+  },
+  {
+    "revision": "cfb3c5302c9cce826f6a",
+    "url": "/next-games-challenge/static/css/main.70aa70b5.chunk.css"
+  },
+  {
+    "revision": "232854dd7071d6310b39",
+    "url": "/next-games-challenge/static/js/2.105f7133.chunk.js"
+  },
+  {
+    "revision": "cfb3c5302c9cce826f6a",
+    "url": "/next-games-challenge/static/js/main.62002136.chunk.js"
+  },
+  {
+    "revision": "0c68e3165f0ba7cf9c07",
+    "url": "/next-games-challenge/static/js/runtime-main.06e13db3.js"
+  }
+]);
